1c1804c045010b8a30fe6b8e79d3e80553dc03cb
Miroslav Maksimović <maksimovic.miroslav999@gmail.com>
2023-09-05 11:20:18 +0200

    Adding Player Movement

---
+++ Assets/Scripts/Core/Player/PreviousMovementInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviousMovementInput : MonoBehaviour
+{
+    Vector2 previousMovementInput;
+
+    public void HandleMove(Vector2 movementInput)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
